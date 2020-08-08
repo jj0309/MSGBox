2cfc2ddc111054cbf37ef910dcd1f33769f2806c
ka-son chau <ka.alex.chau@gmail.com>
2020-08-08 19:51:44 -0400

    login navbar and register frontend

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
     res.render('../public/index.ejs');
+});
+app.get('/login',(req,res)=>{
+    res.render('../public/login/login.ejs');
+})
+app.get('/register',(req,res)=>{
+    res.render('../public/register/register.ejs');
 })
 
 app.listen(80,console.log('running on port 80'));
