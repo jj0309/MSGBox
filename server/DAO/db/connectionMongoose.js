905977ca6a3eb779500e8b31d2f9a867027c071b
ka-son chau <ka.alex.chau@gmail.com>
2020-08-23 14:05:00 -0400

    added connection env var of externaldb

--- server/DAO/db/connectionMongoose.js
+++ server/DAO/db/connectionMongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/MSGBox',{
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/MSGBox',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true,
