9390c117c398f23e7a3a71447734d929083746e8
ka-son chau <ka.alex.chau@gmail.com>
2020-08-29 01:34:44 -0400

    edit informations toggle boxes frontend

--- public/myAccount/js/myAccount.js
+++ public/myAccount/js/myAccount.js
@@ -1,4 +1,4 @@
-const showEditPannels=()=>{
+const showEditPannel=()=>{
     const nodeARR = document.querySelectorAll('.UserPannelDisplay');
     const toHideNodeARR = document.querySelectorAll('.onEditHide');
     nodeARR.forEach(node => {
@@ -7,4 +7,18 @@ const showEditPannels=()=>{
     toHideNodeARR.forEach(node=>{
         node.style.display='none';
     })
+}
+
+// not DRY but bootstrap uses inline-block .... unpredictable display when we modify the style ...
+const hideEditPannel=()=>{
+    const hideARR = document.querySelectorAll('.UserPannelDisplay');
+    const toHideNodeARR = document.querySelectorAll('.onEditHide');
+
+    hideARR.forEach(node=>{
+        node.style.display='none';
+    })
+    toHideNodeARR.forEach(node=>{
+        node.style.display='inline-block';
+    })
+    document.querySelector('.Description').style.display = 'block';
 }
